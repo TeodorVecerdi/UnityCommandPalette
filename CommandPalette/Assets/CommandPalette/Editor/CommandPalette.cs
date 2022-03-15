09180220665e92ddddc03c9604a4c4a2c1418b0c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-03-15 14:17:22 +0200

    Move CommandPalette to correct directory

--- CommandPalette/Assets/CommandPalette/Editor/CommandPalette.cs
+++ CommandPalette/Assets/CommandPalette/Editor/CommandPalette.cs
@@ -1,6 +1,6 @@
 ﻿using CommandPalette.Plugins;
 
-namespace CommandPalette.Commands {
+namespace CommandPalette {
     public static class CommandPalette {
         public static void RegisterPlugin(IPlugin plugin) => PluginManager.Register(plugin);
         public static bool UnregisterPlugin(IPlugin plugin) => PluginManager.Unregister(plugin);
