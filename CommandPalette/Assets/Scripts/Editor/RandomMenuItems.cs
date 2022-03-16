aa665a2d1d8f66cbc3c406871071eacc30250f61
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-03-16 18:08:33 +0200

    Add MenuItems class

---
+++ CommandPalette/Assets/Scripts/Editor/RandomMenuItems.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    public static class RandomMenuItems {
+        [MenuItem("Tools/Find All Windows", false, 101)]
+        private static void FindAllWindows() {
+            EditorWindow[] windows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+            foreach (EditorWindow window in windows) {
+                Debug.Log($"{window.titleContent} ({window.GetType()})");
+            }
+        }
+    }
+}
