a668845c189f5545818494157e353d6d74138d78
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-11-17 11:33:51 +0200

    Move Command Palette settings location to package directory

--- CommandPalette/PluginSupport/PluginSettingsManager.cs
+++ CommandPalette/PluginSupport/PluginSettingsManager.cs
@@ -8,14 +8,11 @@ using Object = UnityEngine.Object;
 
 namespace CommandPalette.Plugins {
     internal static class PluginSettingsManager {
-        private static readonly Dictionary<IPluginSettingsProvider, ScriptableObject> s_Settings = new Dictionary<IPluginSettingsProvider, ScriptableObject>();
-
-        internal static Dictionary<IPluginSettingsProvider, ScriptableObject> Settings => s_Settings;
+        internal static Dictionary<IPluginSettingsProvider, ScriptableObject> Settings { get; } = new();
 
         internal static ScriptableObject GetOrCreateSettings(IPlugin plugin, Type settingsType) {
-            Object[] allAssets = AssetDatabase.LoadAllAssetsAtPath(CommandPaletteSettings.SETTINGS_PATH);
-            ScriptableObject settings = allAssets.FirstOrDefault(x => x.GetType() == settingsType) as ScriptableObject;
-            if (settings is not { }) {
+            Object[] allAssets = AssetDatabase.LoadAllAssetsAtPath(CommandPaletteSettings.GetSettingsPath());
+            if (allAssets.FirstOrDefault(x => x.GetType() == settingsType) is not ScriptableObject settings) {
                 Debug.Log($"<b>CommandPalette Settings Manager</b>: Creating settings object for plugin <b>{plugin?.Name ?? settingsType.Name}</b>");
                 settings = ScriptableObject.CreateInstance(settingsType);
                 settings.name = settingsType.Name;
@@ -27,18 +24,18 @@ namespace CommandPalette.Plugins {
         }
 
         internal static void RegisterSettingsProvider(IPluginSettingsProvider settingsProvider, ScriptableObject settings) {
-            s_Settings[settingsProvider] = settings;
+            Settings[settingsProvider] = settings;
         }
 
         internal static void CleanupAssets() {
             CommandPaletteSettings mainAsset = CommandPaletteSettings.GetOrCreateSettings();
-            Object[] allAssets = AssetDatabase.LoadAllAssetsAtPath(CommandPaletteSettings.SETTINGS_PATH);
+            Object[] allAssets = AssetDatabase.LoadAllAssetsAtPath(CommandPaletteSettings.GetSettingsPath());
             foreach (Object asset in allAssets) {
                 if (asset is not ScriptableObject scriptableObject || asset == mainAsset) {
                     continue;
                 }
 
-                if (s_Settings.ContainsValue(scriptableObject)) {
+                if (Settings.ContainsValue(scriptableObject)) {
                     continue;
                 }
 
@@ -49,7 +46,7 @@ namespace CommandPalette.Plugins {
         }
 
         internal static ScriptableObject GetSettings(IPluginSettingsProvider settingsProvider) {
-            if (s_Settings.TryGetValue(settingsProvider, out ScriptableObject settings)) {
+            if (Settings.TryGetValue(settingsProvider, out ScriptableObject settings)) {
                 return settings;
             }
 
