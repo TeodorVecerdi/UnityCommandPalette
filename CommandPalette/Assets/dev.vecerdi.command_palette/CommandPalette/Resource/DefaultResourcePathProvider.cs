4f1397c5bff7bd8058421b748b15863f8d0ee806
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-11-19 19:21:06 +0200

    Add resource locator

---
+++ CommandPalette/Assets/dev.vecerdi.command_palette/CommandPalette/Resource/DefaultResourcePathProvider.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+
+using System.IO;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace CommandPalette.Resource {
+    internal class DefaultResourcePathProvider : IResourcePathProvider {
+        public string GetResourcePath(string path) {
+            return Path.Combine(CommandPalettePackageLocator.GetCommandPaletteAssetPath(), "EditorResources", path).Replace("\\", "/");
+        }
+    }
+}
